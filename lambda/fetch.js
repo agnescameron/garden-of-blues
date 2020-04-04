a8b128be5a6475e2498d8be58138ff2d89005258
agnescameron <agnesfcameron@gmail.com>
2020-04-04 19:35:06 -0400

    gets recipes

--- lambda/fetch.js
+++ lambda/fetch.js
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch';
-const process_api_key = process.env.AIRTABLE_API_KEY;
+const process_api_key = process.env.NANCY_SEMINE_AIRTABLE_KEY;
 const MAX_RECORDS = 50;
 
 exports.handler = async (event) => {
@@ -7,7 +7,7 @@ exports.handler = async (event) => {
   const { httpMethod } = event;
   
   if (httpMethod === 'GET') {
-	const response = await fetch(`https://api.airtable.com/v0/appJVRlTLOZYKPWBs/PROJECTS?maxRecords=${MAX_RECORDS}&view=Grid%20view`, {
+	const response = await fetch(`https://api.airtable.com/v0/app379cZrnZxEWyFK/Recipes?maxRecords=${MAX_RECORDS}&view=Grid%20view`, {
  	"headers": {"Authorization": `Bearer ${process_api_key}`} })
  	const data = await response.text();
 	return { statusCode: 200, body: data };
