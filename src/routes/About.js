e336785ba1f1837e7be08281bed7768a2a53e45e
agnescameron <agnesfcameron@gmail.com>
2020-04-05 01:22:32 -0400

    home/about routes, now linotype

--- src/routes/About.js
+++ src/routes/About.js
@@ -3,7 +3,16 @@ import React from 'react';
 class About extends React.Component{
 
   render() {
-  	return ( <div>about</div> );
+  	return ( 
+  		<div>
+  			<h1>nancy and semine’s diaspora blues</h1>
+  			<h2>meals from our garden of friendship</h2>
+	  		<div>Our recipes are born out of love for each other. 
+	  		Like Spongebob’s Bikini Bottom, our Zucchini Bottom is a true Garden of Friendship 
+	  		where we nourish and nurture each other in times of heartache, homesickness, finals, 
+	  		and other pains part of this journey that is life. 
+	  		</div>
+  		</div>);
 	}
 
 }
