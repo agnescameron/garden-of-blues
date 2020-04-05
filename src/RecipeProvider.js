a13dbcc05634a4f0f3192e863d9f5e63ea9b1aa7
agnescameron <agnesfcameron@gmail.com>
2020-04-05 00:04:08 -0400

    made some routes

--- src/RecipeProvider.js
+++ src/RecipeProvider.js
@@ -29,7 +29,6 @@ class RecipeProvider extends React.Component {
         return (
             <RecipeContext.Provider value={recipes}>
                 {this.props.children}
-          <div>"hello"</div>
             </RecipeContext.Provider>
         );
     }
---
+++ src/routes/Recipes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Recipes extends React.Component{
+
+  render() {
+  	return ( <div>recipes</div> );
+	}
+
+
+}
+
+export default Recipes;
