bfdfca9e2b48946317fed8e2bf8b594d7d7cb0b6
agnescameron <agnesfcameron@gmail.com>
2020-04-05 00:41:36 -0400

    cleaning up + recipes render

--- src/routes/Recipes.js
+++ src/routes/Recipes.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import RecipeContext from '../RecipeContext';
+import RecipeCard from '../components/RecipeCard';
 
 class Recipes extends React.Component{
 
   render() {
-  	return ( <div>recipes</div> );
+  	return ( 
+
+        <RecipeContext.Consumer>
+          {recipes => { 
+            return (
+              recipes.map((recipe, i) => {
+              // Return the element. Also pass key
+                return (<RecipeCard key={i} index={i} recipe={recipe} />)
+              })
+              )
+          }}
+        </RecipeContext.Consumer>
+      
+  		 );
 	}
 
 
