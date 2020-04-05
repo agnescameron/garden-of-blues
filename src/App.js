2af00bacfc1bcc33fc16cb6b7fd386287742473b
agnescameron <agnesfcameron@gmail.com>
2020-04-05 00:04:08 -0400

    made some routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import RecipeProvider from './RecipeProvider';
 import React from 'react';
 import { Helmet } from 'react-helmet';
+
+// routes
+import Home from './routes/Home';
+import Recipes from './routes/Recipes';
+import About from './routes/About';
+
 import './App.css';
 
 class App extends React.Component {
@@ -9,11 +15,21 @@ class App extends React.Component {
   render() {
     return (
       <RecipeProvider>
-        <Helmet>
-          <title>recipe friendship garden</title>
-          <meta name="description" content="nancy and semine's diaspora blues" />
-          <meta name="keywords" content="recipes, honduras, malaysia" />
-        </Helmet>
+        <BrowserRouter>
+          <Helmet>
+            <title>recipe friendship garden</title>
+            <meta name="description" content="nancy and semine's diaspora blues" />
+            <meta name="keywords" content="recipes, honduras, malaysia" />
+          </Helmet>
+
+          <Switch>  
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/recipes" component={Recipes} />
+            {/*<Route path="/:id" component={RecipePage} />*/}
+          </Switch>
+
+        </BrowserRouter>
       </RecipeProvider>
     );
   }
