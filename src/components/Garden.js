7b8de5f8b75934b0dfa3586bfcb65d5cf6dd89e9
agnescameron <agnesfcameron@gmail.com>
2020-06-06 17:13:44 -0400

    made flower linking demo

--- src/components/Garden.js
+++ src/components/Garden.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Flower from './Flower';
+import RecipeContext from '../RecipeContext';
 
 export default class Garden extends React.Component{
 
+  static contextType = RecipeContext;
+
   render() {
+	const recipes = this.context.filter(entry => entry.Gardens ? entry.Gardens.includes(this.props.garden.name) : '')
+	console.log(recipes)
   	return (
   		<div>
   			<p>{this.props.garden.name}</p>
-  			<p>{this.props.garden.flower}</p>
+  			<Flower flower={this.props.garden.flower} recipeURL={recipes.length ? recipes[0].RecipeName.replace(/\W/g, '') : ''}/>
   		</div>);
 	}
 
