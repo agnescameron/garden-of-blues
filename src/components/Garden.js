b6ebb350edd5e13132b1ee0e6690c6d957245fb6
agnescameron <agnesfcameron@gmail.com>
2020-06-07 14:31:36 -0400

    flowers (sort of) randomly scatter in boxes

--- src/components/Garden.js
+++ src/components/Garden.js
@@ -9,12 +9,41 @@ export default class Garden extends React.Component{
 
   render() {
 	const recipes = this.context.filter(entry => entry.Gardens ? entry.Gardens.includes(this.props.garden.name) : '')
-	console.log(recipes)
+	let flowers = [];
+
+	for(let i=0; i<this.props.garden.numFlowers; i++){
+		if(recipes.length){
+			const el = Math.floor(Math.random()*recipes.length);
+			const recipeURL = recipes[el].RecipeName.replace(/\W/g, '');
+			flowers.push({
+				'recipeURL': recipeURL,
+				'position': {
+					'left': `${Math.random()*80 + 5}%`,
+					'top': `${Math.random()*60 + 5}%`,
+				}
+			})
+		}
+		else {
+			flowers.push({
+				'recipeURL': '',
+				'position': {
+					'left': `${Math.random()*80 + 5}%`,
+					'top': `${Math.random()*60 + 5}%`,
+				}
+			})
+		}
+	}
+
   	return (
   		<div>
   			<p>{this.props.garden.name}</p>
-  			<Flower flower={this.props.garden.flower} recipeURL={recipes.length ? recipes[0].RecipeName.replace(/\W/g, '') : ''}/>
-  		</div>);
+  			{ flowers.map( (flower, index) => {
+  				return (
+  				<div className='flower' style={flower.position} key={index} >
+  					<Flower flower={this.props.garden.flower} recipeURL={flower.recipeURL}/>
+  				 </div>)
+  				})}
+  		</div>
+  		);
 	}
-
 }
